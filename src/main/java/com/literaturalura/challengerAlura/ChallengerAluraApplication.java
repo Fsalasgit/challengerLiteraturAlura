3cc18d18cf9e535ea325e480a42852d35d9fbd28
Franco Salas <franco_10_65@hotmail.com>
2025-01-02 12:53:59 -0300

    Iniciando challenger

---
+++ src/main/java/com/literaturalura/challengerAlura/ChallengerAluraApplication.java
@@ -0,0 +1,13 @@
+package com.literaturalura.challengerAlura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengerAluraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengerAluraApplication.class, args);
+	}
+
+}
